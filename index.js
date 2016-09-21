944e4161374dba63c41970c4a091fd74cedef92a
Imelda <adlewic@gmail.com>
2016-09-21 12:49:31 -0500

    favicon

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
   $('#fullpage').fullpage({
   	 anchors: ['homee', 'projectss', 'aboutt', 'contactss'],
-  	 menu: '#nav_active_menus',
   	 showActiveTooltip: false,
   	 slidesNavigation: true,
   	 slidesNavPosition: 'bottom'
